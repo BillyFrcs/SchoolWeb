dc6ffdadbe9bc66675ef3211b78761da03288691
Billy Franscois <BillyFranscois@outlook.com>
2022-07-05 19:28:45 +0900

    Update Map Controller Route

--- SchoolWeb/Program.cs
+++ SchoolWeb/Program.cs
@@ -9,6 +9,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,8 +21,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
