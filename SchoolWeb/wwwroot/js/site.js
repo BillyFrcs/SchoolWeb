1a7e2a09b1703413e5dc2b0504966f32ae4158f0
BillyFrcs <BillyFranscois@outlook.com>
2022-08-26 12:28:46 +0900

    Adding Scroll Up Button

--- SchoolWeb/wwwroot/js/site.js
+++ SchoolWeb/wwwroot/js/site.js
@@ -6,3 +6,27 @@ $(".nav-link").on("click", (event) => {
     $(".navbar-nav").find(".active").removeClass('active');
     $(event.target).parent().addClass('active');
 });
+
+//Get the button
+let scrollButton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollUpButton();
+};
+
+function scrollUpButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
